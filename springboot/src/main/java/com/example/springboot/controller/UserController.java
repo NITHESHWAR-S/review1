3309ab83112962749d02b7885096d1ef385afb2c
NITHESHWAR-S <117579506+NITHESHWAR-S@users.noreply.github.com>
2024-04-03 12:33:31 +0530

    Add files via upload

---
+++ springboot/src/main/java/com/example/springboot/controller/UserController.java
@@ -0,0 +1,62 @@
+package com.example.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.example.springboot.model.User;
+import com.example.springboot.service.UserService;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserService us;
+
+    @PostMapping("/post")
+public ResponseEntity<List<User>> add(@RequestBody List<User> users) {
+    List<User> newUsers = new ArrayList<>();
+    for (User u : users) {
+        User newUser = us.create(u);
+        newUsers.add(newUser);
+    }
+    return new ResponseEntity<>(newUsers, HttpStatus.CREATED);
+}
+
+    
+    @GetMapping("/getdetails")
+    public ResponseEntity <List<User>> getAllUsers()
+    {
+        List<User>obj = us.getAlldetails();
+        return new ResponseEntity<>(obj,HttpStatus.OK);
+    }
+
+    @PutMapping("/api/user/{userId}")
+    public ResponseEntity<User> putMethodName(@PathVariable("userId") int id, @RequestBody User employee) {
+        if(us.updateDetails(id,employee) == true)
+        {
+            return new ResponseEntity<>(employee,HttpStatus.OK);
+        }
+        
+        return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/api/user/{userId}")
+    public ResponseEntity<Boolean> delete(@PathVariable("userId") int id)
+    {
+        if(us.deleteUser(id) == true)
+        {
+            return new ResponseEntity<>(true,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(false,HttpStatus.NOT_FOUND);
+    }
+
+}
